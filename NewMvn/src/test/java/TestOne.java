9574fcf6eaeb9c731e496950d3922f5118d0375f
DEVMODE <DEVMODE@DEVMODE-PC>
2020-09-10 21:12:45 +0530

    project commit

---
+++ NewMvn/src/test/java/TestOne.java
@@ -0,0 +1,17 @@
+import org.testng.annotations.Test;
+
+public class TestOne {
+	
+	@Test
+	public void test()
+	{
+		System.out.println("testing the case one");
+	}
+	
+	@Test
+	public void test1()
+	{
+		System.out.println("testing the case one");
+	}
+
+}
